24f73ae95972f1814252e626853cdf6d1b0bce0c
stasik <stanislavzmitrovic3@gmail.com>
2024-10-03 10:33:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-n = 12345678
-# Преобразуем число в строку
-digits = str(n)
-# Инициализируем переменную для хранения суммы
-total = 0
-# Используем индексы для доступа к каждой цифре
-total += int(digits[0])
-total += int(digits[1])
-total += int(digits[2])
-total += int(digits[3])
-total += int(digits[4])
-total += int(digits[5])
-total += int(digits[6])
-total += int(digits[7])
-# Выводим рзультат
-print("Сумма цифр числа:", total)
+# Вводим восьмизначное число
+n = int(input("Введите восьмизначное число: "))
+# Инициализируем сумму цифр
+sum_of_digits = 0
+# Вычисляем сумму цифр числа
+sum_of_digits += n // 10000000
+sum_of_digits += (n // 1000000) % 10
+sum_of_digits += (n // 100000) % 10
+sum_of_digits += (n // 10000) % 10
+sum_of_digits += (n // 1000) % 10
+sum_of_digits += (n // 100) % 10
+sum_of_digits += (n // 10) % 10
+sum_of_digits += n % 10
+# Выводим резльтат
+print(f"Сумма цифр числа {n} равна {sum_of_digits}")
